d9e88605b6bf079154c5b9220a9e87b1986a1e30
leoyeh2019 <leoyeh2013@gmail.com>
2019-12-16 19:41:19 +0800

    Lab_5_matplot_1.1

--- practice/0_lab/lab_5/Lab_5_matplot.py
+++ practice/0_lab/lab_5/Lab_5_matplot.py
@@ -16,36 +16,64 @@ def distance(centerA, centerB):
     y = abs(centerA[1] - centerB[1])
     return math.sqrt(x ** 2 + y ** 2)
 
+def checkIntersection(*circles):
+    check = True
+    for cA in circles:
+        if check == False:
+            break
+        for cB in circles:
+            if cA == cB:
+                check = True
+            elif distance(cA.center, cB.center) < abs(cA.radius + cB.radius) and \
+                 distance(cA.center, cB.center) > abs(cA.radius - cB.radius):
+                check = True
+            else: 
+                check = False
+                break
+        
+    return check
 
 ########## Generate Two Circle ##########
-check = True 
+checkA = True 
 count = 0
 
-while check:
+while checkA:
     count += 1
-    c1 = circle(name = "Circle_1", \
-                center = (random.randint(10, 100), random.randint(10, 100)), \
-                radius = random.randint(1, 10), \
-                edgecolor = "r")
-    c2 = circle(name = "Circle_2", \
-                center = (random.randint(10, 100), random.randint(10, 100)), \
-                radius = random.randint(1, 10), \
-                edgecolor = "b")
-    if distance(c1.center, c2.center) < abs(c1.radius + c2.radius) and \
-       distance(c1.center, c2.center) > abs(c1.radius - c2.radius):
-       check = False
+    circleList = []
+    for i in range(1, 4):
+        c = circle(name = "Circle_{}".format(i), \
+                   center = (random.randint(10, 100), random.randint(10, 100)), \
+                   radius = random.randint(1, 10), \
+                   edgecolor = "r")
+        circleList.append(c)
+    
+    # if distance(c1.center, c2.center) < abs(c1.radius + c2.radius) and \
+    #    distance(c1.center, c2.center) > abs(c1.radius - c2.radius):
+    #    checkA = False
+
+    if checkIntersection(circleList[0], circleList[1], circleList[2]):
+        checkA = False
+    
+    # if checkIntersection(circleList):
+    #     checkA = False 
+
+    if count % 500 == 0:
+        print("Please wait for a minute.")
 
 
-print("(x, y) of {0} = {1}".format(c1.name, c1.center))
-print("(x, y) of {0} = {1}".format(c2.name, c2.center))
-print("Iteration of the random processes =", count)
+print("Iteration of the random processes =",count)
+
+for c in circleList:
+    print("(x, y) of {0} = {1}".format(c.name, c.center))
+
 
 ########## Matplot Output ##########
 Lab5_fig = plot.figure("Lab-5")
 figure = Lab5_fig.add_subplot()
     
-figure.add_patch(c1.Circle)
-figure.add_patch(c2.Circle)
+for c in circleList:
+    figure.add_patch(c.Circle)
+
 plot.axis('scaled')
 figure.set_xlim(0, 110)
 figure.set_ylim(0, 110)
